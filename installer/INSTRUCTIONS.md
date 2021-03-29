48c68abc6b2c04f89d4d2441ec3773a32b035b10
Becca Robinson <becca@itgrl.me>
2021-03-28 22:02:17 -0400

    Added in recipe for Workshop 2 - Installing old Tomcat by scratch.  I did this dirty with a lot of execs, but at least they are idempotent.

---
+++ installer/INSTRUCTIONS.md
@@ -0,0 +1,19 @@
+# Becca Robinson WORKSHOP Code
+
+## CentOS/RHEL 7 server
+### Requirements
+* You have a RedHat/CentOS 7 server
+* You have root access or full sudo
+* You have internet access from server to download packages
+
+
+### Instructions
+Simply execute the `installer/installer.sh` script to step through executing the recipes.
+
+## Windows Server 2019
+### Requirements
+* You have a Windows 2019 server
+* You have Administrator access
+* You have internet access from server
+
+### Instructions
---
+++ recipes/mongodb.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook:: WORKSHOPS
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+# configure yum repo
+yum_repository 'mongodb' do
+    description 'MongoDB Repository'
+    baseurl 'http://downloads-distro.mongodb.org/repo/redhat/os/x86_64'
+    action :create
+    gpgcheck false
+    enabled true
+end
+
+# Install mongodb
+package 'mongodb-org' do
+    action :install
+
+end
+
+# Ensure the service is running
+service 'mongod' do
+    action (:running, :enabled)
+end
